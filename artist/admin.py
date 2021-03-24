31ad798f2d97a35f6426f769d695ee9a35feb40a
JoeyChien <39152035+JoeyChien@users.noreply.github.com>
2021-03-24 22:27:54 +0800

    建立Artist的model以及admin site的設定

--- artist/admin.py
+++ artist/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Artist, User
 
-# Register your models here.
+class ArtistInline(admin.StackedInline):
+    model = Artist
+
+class UserAdmin(admin.ModelAdmin):
+    inlines = [ArtistInline]
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
