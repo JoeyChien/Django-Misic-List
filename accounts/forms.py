b3742f22f320d8868a020cf233b40603df42c447
JoeyChien <39152035+JoeyChien@users.noreply.github.com>
2021-03-30 23:07:41 +0800

    建立 accounts 的 app，設定 url，並建立註冊及登入登出的方法

---
+++ accounts/forms.py
@@ -0,0 +1,36 @@
+from django import forms
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class RegisterForm(forms.Form):
+    username = forms.CharField()
+    password1 = forms.CharField(
+        widget = forms.PasswordInput
+    )
+    password2 = forms.CharField(
+        widget = forms.PasswordInput
+    )
+
+    def clean_username(self):
+        username = self.cleaned_data.get("username")
+        qs = User.objects.filter(username__iexact = username)
+        if not qs.exists():
+            raise forms.ValidationError("使用者名稱重複")
+        return username
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(
+        widget = forms.PasswordInput
+    )
+
+    def clean(self):
+        username = self.cleaned_data
+
+    def clean_username(self):
+        username = self.cleaned_data.get("username")
+        qs = User.objects.filter(username__iexact = username)
+        if not qs.exists():
+            raise forms.ValidationError("使用者不存在")
+        return username
--- musicList/urls.py
+++ musicList/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from artist import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('artist.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
