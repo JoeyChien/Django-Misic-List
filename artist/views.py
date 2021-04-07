8c2f6d4169220b711091b6abcfba520f5d9aabb0
JoeyChien <39152035+JoeyChien@users.noreply.github.com>
2021-04-07 22:46:05 +0800

    修改新增 artist 與當下登入的使用者的關聯(原先 artist 的 user 寫死)，並將首頁列出的 artist 改為只顯示登入使用者所新增的 artist

--- artist/views.py
+++ artist/views.py
@@ -9,10 +9,12 @@ def home(request):
     if request.method == 'POST':
         form = ArtistForm(request.POST or None)
         if form.is_valid():
-            form.save()
+            obj = form.save(commit = False)
+            obj.user = request.user
+            obj.save()
             messages.success(request, ('新增成功'))
         else:
             messages.success(request, ('新增失敗'))
 
-    all_artists = Artist.objects.order_by('-created_at')
+    all_artists = Artist.objects.filter(user = request.user).order_by('-created_at')
     return render(request, 'artist/home.html', {'all_artists': all_artists})
